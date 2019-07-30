4ce4cd9aca159427ccd03269ccf4d98f4943da34
vincentni-jrvs <vincent.ni.jrvs.ca@gmail.com>
2019-07-30 14:11:35 +0000

    miscount feq 5 questions by 1. add up the missing question.

---
+++ src/main/java/ca/jrvs/challenge/freq5/Queue_Stack.java
@@ -0,0 +1,12 @@
+package ca.jrvs.challenge.freq5;
+
+public class Queue_Stack {
+    //Difference between Stack and Queue data structure
+    //A queue is a linear data structure in which elements can be inserted only from one side of the list,
+    // and the elements can be deleted only from the other side called the front. This is a FIFO structure.
+    //Insertion and deletion in queues takes place from the opposite ends of the list.
+
+    //Stacks in the other hand are based on the LIFO principle.
+    // The element inserted at the last, is the first element to come out of the list.
+    //Insertion and deletion in stacks takes place only from one end of the list called the top.
+}
